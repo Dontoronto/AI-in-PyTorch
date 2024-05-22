376fba8e645a4590debae58238053229aed0da80
Dontoronto <Dominik.Knauer@gmx.de>
2024-05-22 18:27:14 +0200

    new env.yml

--- selfCoded/evaluationFramework/requirements/cloneDependencys.sh
+++ selfCoded/evaluationFramework/requirements/cloneDependencys.sh
@@ -1,4 +1,4 @@
 echo "Creating the dependency files in current folder"
-
+conda list --export > root-env.txt
 conda env export > environment.yml
 pip freeze | grep -vE "(git+|http+|file+)" > requirements.txt
